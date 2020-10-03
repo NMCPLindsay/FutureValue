33fccad3ff096e14fab23bf01d143add2dd19e00
phil Lindsay <philiplindsay25@gmail.com>
2020-10-03 07:37:07 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FutureValue.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Phil";
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+                
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+    }
+}
